fa41fb61cac1b32521991c213f4475fddcd252eb
stefanie <2968189958@qq.com>
2025-11-22 08:21:29 +0800

    wifi配网

--- main/bsp/bsp_board_wifi.c
+++ main/bsp/bsp_board_wifi.c
@@ -1,8 +1,11 @@
 #include "bsp_board.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
-
+#include "wifi_provisioning/manager.h"
+#include "wifi_provisioning/scheme_ble.h"
+#include "qrcode.h"
 #define TAG "[BSP] WiFi"
+#define SECURITY_KEY "abcd1234"
 static void event_handler(void *event_handler_arg,
                                          esp_event_base_t event_base,
                                          int32_t event_id,
@@ -22,6 +25,13 @@ static void event_handler(void *event_handler_arg,
         ESP_LOGI(TAG, "Got IP:" IPSTR, IP2STR(&event->ip_info.ip));
         //设置WiFi连接成功标志位
         xEventGroupSetBits(board->board_status, BSP_BOARD_WIFI_BIT);
+   
+}  else if(event_base == WIFI_PROV_EVENT && event_id == WIFI_PROV_CRED_FAIL){
+        ESP_LOGE(TAG, "WiFi provisioning failed");
+        wifi_prov_mgr_reset_sm_state_on_failure();
+}  else if(event_base == WIFI_PROV_EVENT && event_id ==WIFI_PROV_END ){
+        wifi_prov_mgr_deinit();
+       
 }
 }
 
@@ -48,24 +58,48 @@ void bsp_board_wifi_init(bsp_board_t *board){
                                                         event_handler,
                                                         board,
                                                         NULL)) ;
-
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_PROV_EVENT,
+         ESP_EVENT_ANY_ID, event_handler, board, NULL));
     //创建wifitask
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    //设置wifi工作模式
-     wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = "iphone",
-            .password = "stefanie1.",
-        },
+      // 初始化配网manager
+    wifi_prov_mgr_config_t config = {
+        .scheme = wifi_prov_scheme_ble,
+        .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM,
     };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-    //启动wifi
-    ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
+     // 检查是否已经配网
+    bool provisioned = false;
+    wifi_prov_mgr_reset_provisioning();
+    ESP_ERROR_CHECK(wifi_prov_mgr_is_provisioned(&provisioned));
+    if (!provisioned)
+    {
+         // 没有配网，启动配网
+        // 通过mac地址获取默认的服务名称
+        uint8_t eth_mac[6];
+        char service_name[15];
+        esp_wifi_get_mac(ESP_IF_WIFI_STA, eth_mac);
+        snprintf(service_name, sizeof(service_name), "PROV_%02X%02X%02X", eth_mac[3], eth_mac[4], eth_mac[5]); // 将mac地址的最后三个字节作为服务名称
+        wifi_prov_mgr_start_provisioning(WIFI_PROV_SECURITY_1, SECURITY_KEY, service_name, NULL);
+        //获取二维码字符串
+         char payload[150];
+        snprintf(payload, sizeof(payload), "{\"ver\":\"v1\",\"name\":\"%s\",\"pop\":\"%s\",\"transport\":\"ble\"}", service_name, SECURITY_KEY);
 
+        ESP_LOGI(TAG, "https://espressif.github.io/esp-jumpstart/qrcode.html?data=%s", payload);
+        //生成qrcode
+        esp_qrcode_config_t cfg = ESP_QRCODE_CONFIG_DEFAULT();
+        //控制台显示二维码
+        esp_qrcode_generate(&cfg, payload);
+        
 
+        }  else{
+            ESP_LOGI(TAG, "Device already provisioned, starting WiFi STA");
+            // 配置WiFi
+            ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 
-
+            // 启动wifi
+            ESP_ERROR_CHECK(esp_wifi_start());
+        }
 
 }
